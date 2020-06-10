84637aa57ed88326e1f21865fb33318676a82045
benmcwilliams <66636868+benmcwilliams@users.noreply.github.com>
2020-06-10 18:04:18 +0100

    add x labels

--- app.py
+++ app.py
@@ -82,6 +82,9 @@ dict ={'AT': 'Austria',
     'JPN':'Japan'
        }
 
+x_labels=['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8',
+          'Week 9', 'Week 10','Week 11','Week 12','Week 13','Week 14']
+
 y_labels = []
 for i in df_default.index.tolist():
     value=dict[i]
