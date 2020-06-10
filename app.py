9216116c67325e23f96dade517dc0424b5a1b8fd
benmcwilliams <66636868+benmcwilliams@users.noreply.github.com>
2020-06-10 16:33:19 +0100

    updates

--- app.py
+++ app.py
@@ -19,50 +19,12 @@ df_default=df_week[df_week.index.isin(['FR','DE','IT','PL','ES','GB'])]
 x_labels=['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8',
           'Week 9', 'Week 10','Week 11','Week 12','Week 13','Week 14']
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
-
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-
-
 ########### Initiate the app
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__)
 server = app.server
 #app.title=tabtitle
 
-########### Set up the layout
 ########### Set up the layout
 app.layout = html.Div([
     html.H1('Weekly Electricity Consumption as % of 2019'),
