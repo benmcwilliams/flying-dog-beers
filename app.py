3c319162ada8d5d81d6d640c29b5a9a4c92f3ec6
benmcwilliams <66636868+benmcwilliams@users.noreply.github.com>
2020-07-20 11:50:02 +0100

    latest data - 20/07

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import plotly.figure_factory as ff
 import numpy as np
 
 #######Clean data
-url = 'https://raw.githubusercontent.com/benmcwilliams/flying-dog-beers/master/df_week_5.csv'
+url = 'https://raw.githubusercontent.com/benmcwilliams/flying-dog-beers/master/df_week_10.csv'
 df_week = pd.read_csv(url, index_col=0)
-df_week=df_week.sort_values(['28'],ascending=False)
+df_week=df_week.sort_values(['29'],ascending=False)
 
 z_labels_df=df_week.fillna(999)
 z_labels=z_labels_df.applymap(lambda x: round(x))
@@ -22,7 +22,7 @@ z_labels_default = z_labels[z_labels.index.isin(default)]
 
 x_labels=['(Mar) Week 1', 'Week 2', 'Week 3', 'Week 4', '(Apr) Week 5', 'Week 6', 'Week 7', 'Week 8',
           'Week 9', '(May) Week 10','Week 11','Week 12','Week 13','(Jun) Week 14','Week 15','Week 16','Week 17',
-         '(July) Week 18','Week 19']
+         '(July) Week 18','Week 19','Week 20]
 
 ctry_labels ={
     'Europe':'European Average',
