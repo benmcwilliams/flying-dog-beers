58b153c3c8248c08958d870643429810cb6c9d74
benmcwilliams <66636868+benmcwilliams@users.noreply.github.com>
2020-10-05 17:24:13 +0200

    increase width

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ def update_graph(dropdown,colour):
       family="Georgia"
     ),
     height=500 + 12.5*len(dropdown),
-    width=1000,
+    width=1100,
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)',
     xaxis_showgrid=False, yaxis_showgrid=False)
