6e2c345f130c10889fcd971e0f8049a22bb7211d
benmcwilliams <66636868+benmcwilliams@users.noreply.github.com>
2021-01-04 18:10:19 +0100

    latest data + Bolivia 04/01

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import plotly.figure_factory as ff
 import numpy as np
 
 #######Clean data
-url = 'https://raw.githubusercontent.com/benmcwilliams/flying-dog-beers/master/df_week_40.csv'
+url = 'https://raw.githubusercontent.com/benmcwilliams/flying-dog-beers/master/df_week_43.csv'
 df_week = pd.read_csv(url, index_col=0)
-df_week=df_week.sort_values(['49'],ascending=False)
+df_week=df_week.sort_values(['52'],ascending=False)
 
 z_labels_df=df_week.fillna(999)
 z_labels=z_labels_df.applymap(lambda x: round(x))
@@ -24,7 +24,7 @@ x_labels=['(Mar) Week 1', 'Week 2', 'Week 3', 'Week 4', '(Apr) Week 5', 'Week 6'
           'Week 9', '(May) Week 10','Week 11','Week 12','Week 13','(Jun) Week 14','Week 15','Week 16','Week 17',
          '(July) Week 18','Week 19','Week 20','Week 21','Week 22','(Aug) Week 23','Week 24','Week 25','Week 26',
          '(Sep) Week 27','Week 28','Week 29','Week 30','(Oct) Week 31','Week 32','Week 33','Week 34','Week 35',
-          '(Nov) Week 36','Week 37','Week 38','Week 39','(Dec) Week 40']
+          '(Nov) Week 36','Week 37','Week 38','Week 39','(Dec) Week 40','Week 41','Week 42','Week 43']
 
 ctry_labels ={
     'Europe':'European Average',
@@ -76,6 +76,8 @@ ctry_labels ={
     'SW':'Southwest (USA)',
     'TEN':'Tennessee (USA)',
     'TEX':'Texas (USA)',
+          
+    'BLV': 'Bolivia',
        
     'NSW':'New South Wales (Aus)',
     'QLD':'Queensland (Aus)',
