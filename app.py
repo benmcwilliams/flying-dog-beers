bf1949dd3dc4f61e69b30c01235e4912fc0015a9
benmcwilliams <66636868+benmcwilliams@users.noreply.github.com>
2021-01-29 16:55:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ def update_graph(dropdown,colour):
       family="Georgia"
     ),
     height=500 + 12.5*len(dropdown),
-    width=1300,
+    width=1500,
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)',
     xaxis_showgrid=False, yaxis_showgrid=False)
